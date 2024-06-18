53f474e16b4980ad6b6f50de9362b0cd1fd63c70
Jesus_Fuentes <jdsusito56778475@gmail.com>
2024-06-18 16:16:42 -0500

    Segundo ejercicio de repetitivas

---
+++ Repetitivas2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int n,cont=1, multiplo;
+	cout<<"Los multiplos de 7"<<endl;
+	cout<<"--------------------"<<endl;
+	cout<<"Escriba un numero "<<endl;
+	cin>>n;
+	n=n+1;
+	while(cont<n)
+
+
+
+
+}
